5b202a15e716675fa8dd40910b0f74557b82fc84
Kendra Givens <kendralee157@gmail.com>
2025-11-29 15:10:53 -0600

    Added basic metamodel and got a basic plugin and vizualizer working

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# miniRL
+# reinforcement_learning
 ## Installation
-First, install the miniRL following:
+First, install the reinforcement_learning following:
 - [NodeJS](https://nodejs.org/en/) (LTS recommended)
 - [MongoDB](https://www.mongodb.com/)
 
 Second, start mongodb locally by running the `mongod` executable in your mongodb installation (you may need to create a `data` directory or set `--dbpath`).
 
-Then, run `webgme start` from the project root to start . Finally, navigate to `http://localhost:8888` to start using miniRL!
+Then, run `webgme start` from the project root to start . Finally, navigate to `http://localhost:8888` to start using reinforcement_learning!
--- config/config.webgme.js
+++ config/config.webgme.js
@@ -7,21 +7,32 @@ var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
 // The paths can be loaded from the webgme-setup.json
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/Reinforcement_Learning');
 
 
 
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
-
+config.rest.components['BindingsDocs'] = {
+  src: __dirname + '/../node_modules/webgme-bindings/src/routers/BindingsDocs/BindingsDocs.js',
+  mount: 'bindings-docs',
+  options: {}
+};
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
-  'minirl': './src/common'
+  'BindingsDocs': 'node_modules/webgme-bindings/src/routers/BindingsDocs',
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
+  'webgme-bindings': './node_modules/webgme-bindings/src/common',
+  'reinforcement_learning': './src/common'
 };
 
 
-config.mongo.uri = 'mongodb://127.0.0.1:27017/minirl';
+config.mongo.uri = 'mongodb://127.0.0.1:27017/reinforcement_learning';
 validateConfig(config);
 module.exports = config;
